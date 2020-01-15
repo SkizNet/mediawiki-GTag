472bce5e92c5ebcbff1ba093bcc918e484b6447b
Ryan Schmidt <skizzerz@skizzerz.net>
2020-01-15 16:56:35 -0700

    Initial commit

---
+++ GTagHooks.php
@@ -0,0 +1,63 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+
+class GTagHooks {
+	public static function onBeforePageDisplay(OutputPage $out, Skin $sk) {
+		$user = $out->getUser();
+		$config = $out->getConfig();
+		$request = $out->getRequest();
+		$permMan = MediaWikiServices::getInstance()->getPermissionManager();
+
+		$gaId = $config->get('GTagAnalyticsId');
+		$honorDNT = $config->get('GTagHonorDNT');
+		$trackSensitive = $config->get('GTagTrackSensitivePage');
+
+		if ($gaId === '' || !preg_match('/^UA-[0-9]+-[0-9]+$/', $gaId)) {
+			// extension not configured yet or invalid configuration, no-op
+			return;
+		}
+
+		// Determine if this is a sensitive page and we should not track it.
+		if (!$trackSensitive) {
+			$allowed = $out->getAllowedModules(ResourceLoaderModule::TYPE_SCRIPTS);
+			if ($allowed < ResourceLoaderModule::ORIGIN_USER_SITEWIDE) {
+				// the current page is not allowing user-editable modules and we
+				// are configured to not track sensitive pages
+				return;
+			}
+		}
+
+		// Determine if we honor DNT headers
+		if ($honorDNT) {
+			// ensure caches vary by the DNT header so that the tracking code is only sent
+			// via upstream caches to people who have not opted out of tracking
+			$out->addVaryHeader('DNT');
+			$dnt = $request->getHeader('DNT');
+			if ($dnt === '1') {
+				// User has sent the DNT header indicating that they do not wish to be tracked
+				return;
+			}
+		}
+
+		// Determine if the user is exempt from tracking
+		if ($permMan->userHasRight($user, 'gtag-exempt')) {
+			return;
+		}
+
+		// If we get here, the user should be tracked
+		// FIXME: getCSPNonce is deprecated in 1.35+. Once we drop 1.34 support, change this
+		$out->addScript(Html::element('script', [
+			'src' => "https://www.googletagmanager.com/gtag/js?id=$gaId",
+			'async' => true,
+			'nonce' => $out->getCSPNonce()
+		]));
+		$out->addInlineScript(<<<EOS
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '$gaId');
+EOS
+);
+	}
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# GTag Extension for MediaWiki
+
+The GTag extension lets you insert the new Google Analytics
+tracking tag on your MediaWiki site (gtag.js).
+
+## Requirements
+
+- MediaWiki 1.34 or later
+
+## Installation
+
+[Download the file from mwusers.org](https://mwusers.org/files/file/4-gtag/)
+(a free account is required) and extract the file to your
+extensions directory. We recommend that you "follow" the
+download so that you are notified of new updates via email
+when they are released.
+
+To install the extension, add the following to your
+LocalSettings.php file:
+```php
+wfLoadExtension( 'GTag' );
+$wgGTagAnalyticsId = 'UA-XXXXXXXX-X'; // replace with your GA id
+```
+
+## Configuration
+
+In addition to the required `$wgGTagAnalyticsId`, this extension
+features many optional configuration variables that you may add
+to your LocalSettings.php file.
+
+| Variable | Default | Description |
+| -------- | ------- | ----------- |
+| `$wgGTagAnalyticsId` | _none_ | Google Analytics Id, for example `'UA-123456789-1'`. Required. |
+| `$wgGTagHonorDNT` | `true` | If true, honor "Do Not Track" requests from browsers. If false, ignore such requests. |
+| `$wgGTagTrackSensitivePages` | `true` | If true, insert tracking code sensitive pages such as Special:UserLogin and Special:Preferences. If false, no tracking code is added to these pages. |
+
+In addition to these configuration variables, you may assign the
+right `gtag-exempt` to user groups to prevent them from being
+tracked. This can be useful to give to staff groups so that your
+internal users and staff are not tracked, giving you a better
+idea of who is actually using your site. For example:
+```php
+$wgGroupPermissions['sysop']['gtag-exempt'] = true;
+```
+
+## Support
+
+Free community support is available on the mwusers.org forums.
+[Paid support plans](https://mwusers.org/store/category/2-mediawiki-support-subscriptions/)
+are available as well.
